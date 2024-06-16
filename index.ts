f94ca180900656ce4a44b541a27c86a1874098e2
Wenxiao(Roy) Shen <royshen12@gmail.com>
2024-06-16 17:18:10 +0800

    update

--- index.ts
+++ index.ts
@@ -19,10 +19,11 @@ const getPath = (file: string) => {
   for (const [dir, regex] of Object.entries(list)) {
     if (regex.test(file)) {
       let match = dir.match(/S(\d+)/)
-      const season = match ? match[1].padStart(2, '0') : '01'
+      const season = match ? match[1]?.padStart(2, '0') : '01'
+      if (!season) continue
       match = file.match(regex)
       const episode = match ? match[1] : '01'
-      if (!episode) return null
+      if (!episode) continue
       return path.join(storageDir, dir, `S${season}E${episode}${path.extname(file)}`)
     }
   }
@@ -37,8 +38,9 @@ const main = async () => {
     .filter(file => file.isFile())
     .forEach(async file => {
       const target = getPath(file.name)
-      console.log(`${file.name} -> ${target}`)
       if (target) {
+        console.log(`${file.name} ->\n ${target}`)
+        await fs.rename(path.join(targetDir, file.name), target)
       }
     })
 }
