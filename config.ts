8305e94a1110890fa87c97b3fdcf20811cf19f56
Wenxiao(Roy) Shen <royshen12@gmail.com>
2024-06-16 16:46:47 +0800

    update

---
+++ config.ts
@@ -0,0 +1,26 @@
+export const list = {
+  '迷宫饭 (2024)/S1':
+    /\[Nekomoe kissaten&LoliHouse\] Dungeon Meshi - (\d\d) \[WebRip 1080p HEVC-10bit AAC EAC3 ASSx2\]\.mkv/,
+  '单间，光照尚好，附带天使。 (2024)/S1/':
+    /\[ANi\] 單人房、日照一般、附天使。 - (\d\d) \[1080P\]\[Baha\]\[WEB-DL\]\[AAC AVC\]\[CHT\]\.mp4/,
+  '吹响吧！上低音号 (2015)/S3': /\[KitaujiSub\] Hibike! Euphonium 3 \[(\d\d)\]\[WebRip\]\[HEVC_AAC\]\[CHS_JP\]\.mp4/,
+  '老夫老妻重返青春 (2024)/S1':
+    /\[LoliHouse\] Jiisan Baasan Wakagaeru - (\d\d) \[WebRip 1080p HEVC-10bit AAC ASSx2\]\.mkv/,
+  '怪兽8号 (2024)/S1': /\[LoliHouse\] Kaijuu 8-gou - (\d\d) \[WebRip 1080p HEVC-10bit AAC SRTx2\]\.mkv/,
+  '从Lv2开始开外挂的前勇者候补过着悠哉异世界生活 (2024)/S1':
+    /\[LoliHouse\] Lv2 kara Cheat datta Motoyuusha Kouho no Mattari Isekai Life - (\d\d) \[WebRip 1080p HEVC-10bit AAC SRTx2\]\.mkv/,
+  'ガールズバンドクライ (2024)/S1':
+    /\[Nekomoe kissaten&LoliHouse\] GIRLS BAND CRY - (\d\d) \[WebRip 1080p HEVC-10bit AAC ASSx2\]\.mkv/,
+  '怪人的沙拉碗 (2024)/S1':
+    /\[Nekomoe kissaten&LoliHouse\] Henjin no Salad Bowl - (\d\d) \[WebRip 1080p HEVC-10bit AAC ASSx2\]\.mkv/,
+  '身为魔王的我娶了奴隶精灵为妻，该如何表白我的爱？ (2024)/S1':
+    /\[Nekomoe kissaten&LoliHouse\] Madome - (\d\d) \[WebRip 1080p HEVC-10bit AAC ASSx2\]\.mkv/,
+  '恋语轻唱 (2024)/S1':
+    /\[Nekomoe kissaten&LoliHouse\] Sasayaku You ni Koi wo Utau - (\d\d) \[WebRip 1080p HEVC-10bit AAC ASSx2\]\.mkv/,
+  '夜晚的水母不会游泳 (2024)/S1':
+    /\[Nekomoe kissaten&LoliHouse\] Yoru no Kurage wa Oyogenai - 09 \[WebRip 1080p HEVC-10bit AAC ASSx2\]\.mkv/,
+  '声优广播的台前幕后 (2024)/S1': /\[Nekomoe kissaten\]\[Seiyuu Radio no Uraomote\]\[(\d\d)\]\[1080p\]\[JPSC\]\.mp4/,
+  '无名记忆 (2024)/S1': /\[Nekomoe kissaten\]\[Unnamed Memory\]\[(\d\d)\]\[1080p\]\[JPSC\]\.mp4/,
+  '为美好的世界献上祝福！ (2016)/S3':
+    /\[Sakurato\] Kono Subarashii Sekai ni Shukufuku wo! 3 \[(\d\d)\]\[AVC-8bit 1080p AAC\]\[CHS\]\.mp4/,
+}
---
+++ index.ts
@@ -0,0 +1,45 @@
+import { promises as fs } from 'fs'
+
+import { list } from './config'
+import path from 'path'
+
+const targetDir = process.argv[2]
+if (!targetDir) {
+  console.error('target directory is required!')
+  process.exit(1)
+}
+
+const storageDir = process.argv[3]
+if (!storageDir) {
+  console.error('storage directory is required!')
+  process.exit(1)
+}
+
+const getPath = (file: string) => {
+  for (const [dir, regex] of Object.entries(list)) {
+    if (regex.test(file)) {
+      let match = dir.match(/S(\d+)/)
+      const season = match ? match[1] : '01'
+      match = file.match(regex)
+      const episode = match ? match[1] : '01'
+      return path.join(storageDir, dir, `S${season}E${episode}`)
+    }
+  }
+
+  return null
+}
+
+const main = async () => {
+  const files = await fs.readdir(targetDir, { withFileTypes: true })
+
+  files
+    .filter(file => file.isFile())
+    .forEach(async file => {
+      const target = getPath(file.name)
+      if (target) {
+        console.log(`${file} -> ${target}`)
+      }
+    })
+}
+
+main()
